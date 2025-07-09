08436de7a4c40fa1d8c7e86877cdc609f7df398b
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-07-09 20:43:19 +0500

    cart complete

--- src/core/Cart.jsx
+++ src/core/Cart.jsx
@@ -39,7 +39,7 @@ const Cart = () => {
     items.reduce((acc, item) => acc + item.count * item.price, 0).toFixed(2);
 
   return (
-    <Layout>
+    <Layout title="Cart" description={undefined}>
       <div className="container py-4">
         <h3 className="mb-4">You have {items.length} items in cart</h3>
 
