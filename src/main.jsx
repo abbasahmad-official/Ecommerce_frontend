365d139ab145a3fbaaf535725871de3c18f3e3e7
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-07-09 10:16:47 +0500

    again hamburger

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import  Routess  from './Routes'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // <-- this enables dropdowns, modals, navbar toggle, etc.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
