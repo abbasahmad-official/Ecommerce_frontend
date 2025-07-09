e1659fdd1adeee7ba79ca1cbe02abe2577f6f8ad
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-07-09 11:23:51 +0500

    image width

--- src/core/ShowImage.jsx
+++ src/core/ShowImage.jsx
@@ -3,7 +3,7 @@ import { API } from "../config";
 
 const ShowImage = ({item, url}) => {
    return (<div className="product-img">
-        <img src={`${API}/${url}/photo/${item._id}`}  alt={item.name} style={{ objectFit: "cover", height: "200px" }}/>
+        <img src={`${API}/${url}/photo/${item._id}`}  alt={item.name} style={{ objectFit: "cover", height: "200px", width: "100%"}}/>
     </div>)
 }
 
