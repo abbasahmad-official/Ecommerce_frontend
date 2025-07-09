bf211cdfbcaf7cd553f5749d2e37125c4dc5eee2
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-07-09 21:07:54 +0500

    margin end

--- src/core/ShowImage.jsx
+++ src/core/ShowImage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { API } from "../config";
 
-const ShowImage = ({item, url, from=null }) => {
+const ShowImage = ({item, url, from=null, className="" }) => {
    return (<div className="product-img">
-        <img src={`${API}/${url}/photo/${item._id}`}  alt={item.name} style={{ objectFit: "cover", height: from? "70px": "200px"}}/>
+        <img className={`${className}`} src={`${API}/${url}/photo/${item._id}`}  alt={item.name} style={{ objectFit: "cover", height: from? "70px": "200px"}}/>
     </div>)
 }
 
